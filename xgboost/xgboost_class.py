2de6a43205a8d5194cdd9c25905472dc736af11f
张双江 <zsjwish@126.com>
2018-12-28 17:19:01 +0800

    xgboost

---
+++ xgboost/xgboost_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/28 16:35
+# @Author  : zsj
+# @File    : xgboost_class.py
+# @Description:
+
+class Xgboost():
+    pass
---
+++ xgboost/xgboost_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/28 16:38
+# @Author  : zsj
+# @File    : xgboost_test.py
+# @Description:
+
+import xgboost as xgb
+
